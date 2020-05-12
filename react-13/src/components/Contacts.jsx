9a2dd2df5d16cb29c11dacabde4c84b1836c17e4
carolinabranquinho <carol.a.branquinho@gmail.com>
2020-05-12 11:55:39 -0300

    React challenge - part 1

---
+++ react-13/src/components/Contacts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Contacts.scss';
+import Contact from "./Contact";
+
+class Contacts extends React.Component {
+	render() {
+		return (
+		  <div className="container"  data-testid="contacts">
+			<section className="contacts">
+			  <Contact  />
+			</section>
+		  </div>
+		);
+	}
+}
+
+export default Contacts;
